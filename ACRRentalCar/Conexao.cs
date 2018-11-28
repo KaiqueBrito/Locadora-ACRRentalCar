445be7937f33c2dfa5f0d2a3489a032c922a847a
kaiquedsb <kaiquedsb@uninove.edu.br>
2018-11-28 20:22:45 -0200

    projeto locadora terminado

---
+++ ACRRentalCar/Conexao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ACRRentalCar
+{
+    public class Conexao
+    {
+        public static SqlConnection GetConnection()
+        {
+            SqlConnection cnn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=;C: \Users\VM estudo\Desktop\ACRRentalCar\ACRRentalCar\ACRRentalCar\DB_RENTALCAR.mdf;Integrated Security=True;Connect Timeout=30");
+            return cnn;
+        }
+    }
+}
